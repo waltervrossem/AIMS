c12829fedacedf78ca397143a7a7fef9289bacb4
waltervrossem <walter_v_rossem@hotmail.com>
2026-02-20 11:57:47 +0100

    Set default set_mode_n_to_radial_n to false

--- src/AIMS_configure.py
+++ src/AIMS_configure.py
@@ -176,7 +176,7 @@ mode_format   = "MESA"           # specifies the format of the files with
                                  #             along with their pulsation frequencies
 npositive     = True             # if True, only save modes with n >= 0 in
                                  # binary grid file
-set_mode_n_to_radial_n = True    # if True, re-assign non-radial mode orders to their
+set_mode_n_to_radial_n = False   # if True, re-assign non-radial mode orders to their
                                  # corresponding radial mode order
 cutoff        = 5.0              # remove frequencies above this value times
                                  # the acoustic cutoff-frequency
